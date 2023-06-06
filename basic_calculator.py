743b5c41618f06095b6c0a6dccf9a0d261ca5edd
Nick Ochieng <125216499+1nickochieng@users.noreply.github.com>
2023-06-06 22:02:46 +0300

    Add files via upload

--- basic_calculator.py
+++ basic_calculator.py
@@ -13,12 +13,17 @@ def mul(a, b):
 def div(a, b):
     answer = a / b
     print(str(a) + " / " + str(b) + " = " + str(answer))    
+
+def modulus(a % b):
+    answer = a % b
+    print(str(a) + " % " + str(b) + " = " + str(answer))
     
     
 print("A. addition")
 print("B. Subtraction")
 print("C. Multipliction")
 print("D. Division")
+print("F. Modulus")
 print("E. Exit")
 
 choice = input("Input your choice: ")
@@ -43,6 +48,11 @@ elif choice == "d" or choice == "D":
     a = int(input("Input first number: "))
     b = int(input("Input second number: "))
     div(a, b)
+elif choice == "f" or choice == "F":
+    print("Modulus")
+    a = int(input("Input first number: "))
+    b = int(input("Input second number: "))
+    modulus(a, b)
 elif choice == "e" or choice == "E":
     print("Program ended!")
     quit()
